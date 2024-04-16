5396df310897cf968c2fae9e4358f94723bef154
nel4nelson <an.ekwueme@stu.unizik.edu.ng>
2024-04-16 16:35:42 +0100

    implement protected routing

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -1,12 +1,18 @@
 import App from "@/App";
 import LoginPage from "@/Pages/Auth/LoginPage";
 import RegisterPage from "@/Pages/Auth/RegisterPage";
+import PrivateRoutes from "@/components/PrivateRoutes";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/register", element: <RegisterPage /> },
   { path: "/login", element: <LoginPage /> },
+  {
+    element: <PrivateRoutes />,
+    path: "/",
+    children: [{ index: true, element: <App /> }],
+  },
 ]);
 
 export default router;
